b71434dc8e7c2165f1c18b8efea86f345024a3e9
Justin Riggio <nicemindz@gmail.com>
2013-05-23 16:58:43 -0400

    added deck js files

---
+++ package.js
@@ -0,0 +1,32 @@
+Package.describe({
+  summary: "Modern HTML Presentations"
+});
+
+Package.on_use(function (api) {
+  api.use('jquery', 'client');
+  api.add_files([
+    'lib/modernizr.custom.js',
+    'lib/core/deck.core.js',
+    'lib/core/deck.core.css',
+    'lib/extensions/goto/deck.goto.js',
+    'lib/extensions/goto/deck.goto.js',
+    'lib/extensions/hash/deck.hash.js',
+    'lib/extensions/hash/deck.hash.css',
+    'lib/extensions/menu/deck.menu.js',
+    'lib/extensions/menu/deck.menu.css',
+    'lib/extensions/navigation/deck.navigation.js',
+    'lib/extensions/navigation/deck.navigation.css',
+    'lib/extensions/scale/deck.scale.js',
+    'lib/extensions/scale/deck.scale.css',
+    'lib/extensions/status/deck.scale.js',
+    'lib/extensions/status/deck.status.css',
+    'lib/themes/style/neon.css',
+    'lib/themes/style/swiss.css',
+    'lib/themes/style/web-2.0.css',
+    'lib/themes/transition/fade.css', 
+    'lib/themes/transition/horizontal-slide.css',
+    'lib/themes/transition/vertical-slide.css',
+    'lib/delete.gif'
+  ], 'client'
+  );
+});
